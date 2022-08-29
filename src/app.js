33005e9392b2eb2c856485669ed3bd21da4ec43f
Gabriel <gabriel.liradefarias2@gmail.com>
2022-08-29 01:45:53 -0300

    get tweets route

--- src/app.js
+++ src/app.js
@@ -13,19 +13,43 @@ app.post('/sign-up', (req, res) => {
     if(!user.avatar || !user.username){
         return res.sendStatus(400)
     }
+    for(let i = 0; i < users.length; i++){
+        if(user.username === users[i].username){
+            return res.sendStatus(409)
+        }
+    }
     users.push(user)
     return res.sendStatus(200)
 })
 
 app.post('/tweets', (req, res) => {
     const tweet = req.body
+    for(let i = 0; i < users.length; i++){
+        if(tweet.username === users[i].username){
+            tweets.push({...tweet, avatar: users[i].avatar})
+            console.log(tweets)
+            return res.sendStatus(200)
+
+        }
+    }
     if(!tweet.username || !tweet.tweet){
         return res.sendStatus(400)
     }
-
-    tweets.push(tweet)
-    return res.sendStatus(200)
+    return res.sendStatus(400)
 })
+
+app.get('/tweets', (req, res) => {
+    const lastTweets = []
+    if(tweets.length < 10){
+        return res.send(tweets)
+    }
+    for(let i = tweets.length -1 ; i > tweets.length - 11; i --){
+        lastTweets.push(tweets[i])
+    }
+    const result = lastTweets.reverse()
+    return res.send(result)
+})
+
 
 const PORT = 5000
 app.listen(PORT, () => {
